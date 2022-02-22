09fe1fbd84e9e15f0c83fef1ac61d85b6070e12b
yuanhenglee <1990henrylee@gmail.com>
2022-02-22 20:12:30 +0800

    add: turn runall's output to one file

--- runAll.py
+++ runAll.py
@@ -7,8 +7,7 @@ inputDir = ''
 outputDir = ''
 
 def appendToFile( outputDir, txt ):
-    print("echo \"" + txt + "\" >> " + outputDir)
-    os.system("echo \"" + txt + "\" >> " + outputDir )
+    os.system("echo \"\\n" + txt + "\" >> " + outputDir )
 
 def runSingleFile(fileDir, inputDir = inputDir, outputDir = outputDir):
     path, file = os.path.split(fileDir)
@@ -19,18 +18,15 @@ def runSingleFile(fileDir, inputDir = inputDir, outputDir = outputDir):
         os.system("jupyter nbconvert --to script " + fileDir)
         fileDir = os.path.join(path, filename + ".txt")
 
-    codeFile = open(fileDir, 'r')
-    inputFile = open(inputDir, 'r')
-    outputFile = open(outputDir, 'w')
+    outputFile = os.path.join(outputDir, filename + ".output")
 
-
-    appendToFile( outputDir, "#"*5 + "source code start" + "#"*5 )
-    os.system("cat " + fileDir + " >> " + outputDir)
-    appendToFile( outputDir, "#"*5 + "source code end" + "#"*5 )
+    appendToFile( outputFile, "#"*5 + "source code start" + "#"*5 )
+    os.system("cat " + fileDir + " >> " + outputFile)
+    appendToFile( outputFile, "#"*5 + "source code end" + "#"*5 )
     
-    appendToFile( outputDir, "#"*5 + "output" + "#"*5 )
-    os.system("python3 "+ fileDir + " < " + inputDir + " >> " + outputDir + " 2>&1 ")
-    appendToFile( outputDir, "#"*5 + "output" + "#"*5 )
+    appendToFile( outputFile, "#"*5 + "output" + "#"*5 )
+    os.system("python3 "+ fileDir + " < " + inputDir + " >> " + outputFile + " 2>&1 ")
+    appendToFile( outputFile, "#"*5 + "output" + "#"*5 )
 
 
 if __name__ == '__main__':
@@ -43,18 +39,13 @@ if __name__ == '__main__':
 
     print("code dir:",codeDir)
 
-    runSingleFile(codeDir, inputDir, outputDir)
-    # for root, dirs, files in os.walk(codeDir):
-    #     print( "Dir: ", root)
-    #     for filename in files:
-    #         filePath = os.path.join(root, filename)
-    #         os.rename( filePath, filePath.encode('utf-8') )
-    #         if os.path.splitext(filePath)[1] in fileTypes:
-    #             print("└ Match file: ", filePath)
-    #             handle_file(os.path.abspath(filePath))
-
-    #         else:
-    #             print("└ Unknown file: ", filePath)
-
-
-    # os.system('python file.py')
+    for root, dirs, files in os.walk(codeDir):
+        print( "Dir: ", root)
+        for filename in files:
+            filePath = os.path.join(root, filename)
+            os.rename( filePath, filePath.encode('utf-8') )
+            if os.path.splitext(filePath)[1] in fileTypes:
+                print("└ Match file: ", filePath)
+                runSingleFile( filePath, inputDir, outputDir )
+            else:
+                print("└ Unknown file: ", filePath)
